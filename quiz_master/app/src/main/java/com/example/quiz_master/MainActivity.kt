9892ec714917b0ec274a01b53b0350a8cacb22f3
Jayasubin J <subin@crestronics.com>
2022-09-06 11:13:25 +0530

    module 8 in progress

---
+++ quiz_master/app/src/main/java/com/example/quiz_master/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.quiz_master
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn_start: Button =findViewById(R.id.button_start)
+
+    }
+}
